8d686001d92a36abc01e460992f247fbf7ef0ada
yamilavila <git@github.com>
2022-08-12 20:39:52 +0000

    Task 0, creating a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Using puppet this code is for creating a file
 
 file { 'school':
-ensure   => 'file',
-path     => '/tmp/school',
-mode     => '0744',
-owner    => 'www-data',
-group    => 'www-data',
-content  => 'I love Puppet',
+ensure  => 'file',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
