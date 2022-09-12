dda19bd562055fa7c17455d73eda1729f5d3d65b
yamilavila <yamilpra@hotmail.com>
2022-09-12 19:20:50 -0400

    Task 0, gathering data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,12 @@ from sys import argv
 if __name__ == '__main__':
 
     employee_id = argv[1]
-    user = request.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user = request.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(employee_id))
 
     name_employee = user.jason()['name']
-    todo = request.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+    todo = request.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                       .format(employee_id))
 
     todo = todo.json()
 
@@ -22,7 +24,8 @@ if __name__ == '__main__':
             num_done_task += 1
             task_list.append(task['title'])
 
-    print("Employee {} is done with tasks({}/{}):".format(name_employee, num_done_task, len(todo)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name_employee, num_done_task, len(todo)))
 
     for done in range(len(task_list)):
         print("\t {}".format(task_list[done]))
