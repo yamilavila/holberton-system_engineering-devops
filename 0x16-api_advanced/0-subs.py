a90cae4b7dd61f650920966588148eee24256cb7
yamilavila <yamilpra@hotmail.com>
2022-09-27 21:27:11 -0400

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""task 0: how many subs """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    api_header = { 'User-Agent': 'Mozilla/5.0'}
+
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    api_res = request.get(api_url, headers=api_header, allow_redirects=False)
+    if api_res.status_code != 200:
+        return 0
+    api_json = api_res.json()
+
+    return api_json['data']['subscribers']
